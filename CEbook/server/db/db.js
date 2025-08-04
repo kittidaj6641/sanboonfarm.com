c3735f5f1b95a83cf114b46caf7a03e1a7171c73
Kittidaj <tony@example.com>
2025-08-04 08:33:06 +0700

    แก้ไข package.json และเพิ่มสคริปต์ start, dev

--- CEbook/server/db/db.js
+++ CEbook/server/db/db.js
@@ -1,39 +1,12 @@
-import pkg from 'pg'
+import pkg from 'pg';
+import dotenv from 'dotenv';
 
-import dotenv from 'dotenv'
-
-
-
-dotenv.config()
-
-const { Pool } = pkg
-
-
-
-//สร้างคอนเนคชั่น
+dotenv.config();
+const { Pool } = pkg;
 
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // ใช้ Connection String แทนแยกค่า
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+});
 
-  user: process.env.DB_USER,
-
-  host: process.env.DB_HOST,
-
-  database: process.env.DB_NAME,
-
-  password: process.env.DB_PASSWORD,
-
-  port: process.env.DB_PORT
-
-})
-
-
-
-//ตรวจสอบ
-
-pool.connect()
-
-    .then( () => console.log("เชื่อมต่อ Database สำเร็จ") )
-
-    .catch( err => console.log("เชื่อมต่อ Database ผิดพลาด ", err) )
-
-export default pool
+export default pool;
